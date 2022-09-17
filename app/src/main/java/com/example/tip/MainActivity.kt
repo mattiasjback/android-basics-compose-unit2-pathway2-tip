9960e512e684e954bd72100d76d8c1412c0844c2
Mattias Bäck <mattias.back@bontouch.com>
2022-09-17 10:19:31 +0200

    Add title and input field

--- app/src/main/java/com/example/tip/MainActivity.kt
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.tip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tip.ui.theme.TipTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,19 +22,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TipTheme {
-                TipApp()
+
             }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun TipApp() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-
+        Column(
+            modifier = Modifier.padding(32.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.calculate_tip),
+                fontSize = 24.sp,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            EditTextField()
+        }
     }
+}
+
+@Composable
+fun EditTextField() {
+    TextField(value = "n/a", onValueChange = {})
 }
