bcf1e3da1b57c05d9d4f78dcf2c7f20ce44d347c
Mattias Bäck <mattias.back@bontouch.com>
2022-09-17 10:07:31 +0200

    init

---
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.tip
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tip.ui.theme.TipTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTheme {
+                TipApp()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TipApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+
+    }
+}
