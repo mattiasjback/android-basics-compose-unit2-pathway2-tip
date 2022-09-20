26a34f51493d39ee93d52504ea681f5e5c983cef
Mattias Bäck <mattias.back@bontouch.com>
2022-09-20 09:52:41 +0200

    Add UI Tests

---
+++ app/src/androidTest/java/com/example/tip/TipUITests.kt
@@ -0,0 +1,27 @@
+package com.example.tip
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.example.tip.ui.theme.TipTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipTheme {
+                TipApp()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        composeTestRule.onNodeWithText("Tip Amount: $2.00").assertExists()
+    }
+}
+
